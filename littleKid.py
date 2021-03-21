6fd95cc61c34f62b9d80d394161f2164ebf20f54
jjakobowska <61043020+jjakobowska@users.noreply.github.com>
2021-03-21 14:49:10 +0100

    Create littleKid.py

---
+++ littleKid.py
@@ -0,0 +1,11 @@
+print('what is your name')
+name = input()
+print('What is your age')
+age = int(input())
+
+if name == 'Alice':
+      print('Hi alice')
+elif age < 12:
+    print('a kid')
+else:
+    print('not an alice not a kid')
