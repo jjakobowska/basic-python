922468036bf5bdd4587057a8278787cb40eefaac
jjakobowska <61043020+jjakobowska@users.noreply.github.com>
2021-03-21 14:47:10 +0100

    Create vampire.py

---
+++ vampire.py
@@ -0,0 +1,14 @@
+// use of elif and if 
+print('what is your name')
+name = input()
+print('What is your age')
+age = int(input())
+
+if name == 'Alice':
+      print('Hi alice')
+elif age < 12 :
+        print(' you are a kid')
+elif age > 2000:
+        print('you are a vampire')
+elif age > 100 :
+        print('granny')
